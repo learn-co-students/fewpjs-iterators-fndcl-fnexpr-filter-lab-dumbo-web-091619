e83e962435796fa0b604ddcd29c98690686c5c01
Reina <r.clarkemitchell@gmail.com>
2019-11-17 18:04:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching (arr, name) {
+  const matching = arr.filter(function (findName) {
+    return findName.toLowerCase() === name.toLowerCase()
+  })
+  return matching
+}
+
+function fuzzyMatch (arr, str) {
+  return arr.filter(function (name) {
+    return name.slice(0, 3).includes(str)
+  })
+}
+
+function matchName (arr, str) {
+  return arr.filter(function (name) {
+    return name.name === str
+  })
+}
